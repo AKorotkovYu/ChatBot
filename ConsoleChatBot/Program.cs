c9641af2543ce0e032255cb63b32c8e9f0ec3847
AKorotkovYu <a.korotkov.yu@gmail.com>
2021-05-25 18:11:30 +0300

    Бот

--- ConsoleChatBot/Program.cs
+++ ConsoleChatBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibraryChatBot;
 
 namespace ConsoleChatBot
 {
@@ -6,7 +7,18 @@ namespace ConsoleChatBot
     {
         static void Main(string[] args)
         {
-             
+            Bot bot = new Bot();
+            System.Console.WriteLine(bot.TakeAnswer("Привет"));
+
+            string s = new String("");
+            while (bot.isFinal == false)
+            {
+                System.Console.WriteLine("");
+                s = Console.ReadLine();
+                System.Console.WriteLine("");
+                System.Console.WriteLine(bot.TakeAnswer(s));
+            }
+
         }
     }
 }
